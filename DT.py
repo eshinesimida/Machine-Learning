3179838dd8d4f9487e9a7ba2f37935eab416dd5e
Xianbin Li <454656783@qq.com>
2017-11-14 13:43:18 +0800

    main

--- DT.py
+++ DT.py
@@ -4,6 +4,121 @@ Created on Mon Nov 13 21:02:08 2017
 
 @author: dell
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 21:02:08 2017
+
+@author: dell
+"""
 from math import log
 
 def createDataSet():
+    dataSet = [[0, 0, 0, 0, 'no'],         #数据集
+            [0, 0, 0, 1, 'no'],
+            [0, 1, 0, 1, 'yes'],
+            [0, 1, 1, 0, 'yes'],
+            [0, 0, 0, 0, 'no'],
+            [1, 0, 0, 0, 'no'],
+            [1, 0, 0, 1, 'no'],
+            [1, 1, 1, 1, 'yes'],
+            [1, 0, 1, 2, 'yes'],
+            [1, 0, 1, 2, 'yes'],
+            [2, 0, 1, 2, 'yes'],
+            [2, 0, 1, 1, 'yes'],
+            [2, 1, 0, 1, 'yes'],
+            [2, 1, 0, 2, 'yes'],
+            [2, 0, 0, 0, 'no']]
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']
+    return dataSet, labels
+    
+def calcShannonEnt(dataSet):
+    numEntires = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntires
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+if __name__ == '__main__':
+    dataSet, features = createDataSet()
+    print(dataSet)
+    print(calcShannonEnt(dataSet))
+    
+########information gain
+
+from math import log
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+    
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        print('第%d个特征的增益为%.3f' % (i, infoGain))
+        if(infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+    
+if __name__ == '__main__':
+    dataSet, features = createDataSet()
+    print('最优特征索引值:' + str(chooseBestFeatureToSplit(dataSet)))
+    
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote in classList:
+            if vote not in classCount.keys():
+                classCount[vote] = 0
+            classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True )
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels, featLabels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    elif len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    featLabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels, featLabels)
+    return myTree
+    
+if __name__ == '__main__':
+    dataSet,labels = createDataSet()
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    print(myTree)
+        
+    
---
+++ DT.py.bak
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 21:02:08 2017
+
+@author: dell
+"""
+from math import log
+
+def createDataSet():
