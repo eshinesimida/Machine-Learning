f6d67d1a8b4c4b7bf3c3c01122c6aaa0623429e7
Xianbin Li <454656783@qq.com>
2017-12-26 21:45:18 +0800

    adaboost

---
+++ adaboost.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 18:05:03 2017
+
+@author: dell
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def loadSimpData():
+    datMat = np.matrix([[1., 2.1],
+                        [1.5, 1.6],
+                        [1.3, 1.],
+                        [1., 1.],
+                        [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+    
+def showDataSet(dataMat, labelMat):
+    data_plus = []
+    data_minus = []
+    for i in range(len(dataMat)):
+        if labelMat[i] > 0:
+            data_plus.append(dataMat[i])
+        else:
+            data_minus.append(dataMat[i])
+    data_plus_np = np.array(data_plus)
+    data_minus_np = np.array(data_minus)
+    plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1])
+    plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1])
+    plt.show()
+    
+if __name__ == '__main__':
+    dataArr, classLabels = loadSimpData()
+    showDataSet(dataArr, classLabels)
+    
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+    retArray = np.ones((np.shape(dataMatrix)[0],1))                #初始化retArray为1
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:,dimen] <= threshVal] = -1.0         #如果小于阈值,则赋值为-1
+    else:
+        retArray[dataMatrix[:,dimen] > threshVal] = -1.0         #如果大于阈值,则赋值为-1
+    return retArray
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = np.mat(dataArr)
+    labelMat = np.mat(classLabels).T
+    m,n = np.shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClasEst = np.mat(np.zeros((m,1)))
+    minError = float('inf')
+    for i in range(n):
+        rangeMin = dataMatrix[:, i].min(); rangeMax = dataMatrix[:,i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1, int(numSteps) + 1):
+            for inequal in ['lt', 'gt']:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = np.mat(np.ones((m, 1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr
+                print("split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError))
+                if weightedError < minError:                                     #找到误差最小的分类方式
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump,minError,bestClasEst
+if __name__ == '__main__':
+    dataArr,classLabels = loadSimpData()
+    D = np.mat(np.ones((5, 1)) / 5)
+    bestStump,minError,bestClasEst = buildStump(dataArr,classLabels,D)
+    print('bestStump:\n', bestStump)
+    print('minError:\n', minError)
+    print('bestClasEst:\n', bestClasEst)
