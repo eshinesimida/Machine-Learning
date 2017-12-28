d9aafadc54bfe67940f6546580b81da6b227f571
Xianbin Li <454656783@qq.com>
2017-12-28 21:32:23 +0800

    adaboost

--- adaboost.py
+++ adaboost.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     print('bestStump:\n', bestStump)
     print('minError:\n', minError)
     print('bestClasEst:\n', bestClasEst)
-
+    
 def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
     weakClassArr = []
     m = np.shape(dataArr)[0]
@@ -106,3 +106,100 @@ if __name__ == '__main__':
     weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, classLabels)
     print(weakClassArr)
     print(aggClassEst)
+    
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = np.mat(datToClass)
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m, 1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'], classifierArr[i]['ineq'])            
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print(aggClassEst)
+    return np.sign(aggClassEst)
+    
+if __name__ == "__main__":
+    dataArr, classLabels = loadSimpData()
+    weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, classLabels)
+    print(adaClassify([[0,0],[5,5]], weakClassArr))
+    
+def loadDataSet(fileName):
+    numFeat = len((open(fileName).readline().split('\t')))
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+    
+if __name__ == '__main__':
+    dataArr, LabelArr = loadDataSet('horseColicTraining2.txt')
+    weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, LabelArr)
+    testArr, testLabelArr = loadDataSet('horseColicTest2.txt')
+    print(weakClassArr)
+    predictions = adaClassify(dataArr, weakClassArr)
+    errArr = np.mat(np.ones((len(dataArr), 1)))
+    print('训练集的错误率:%.3f%%' % float(errArr[predictions != np.mat(LabelArr).T].sum() / len(dataArr) * 100))
+    predictions = adaClassify(testArr, weakClassArr)
+    errArr = np.mat(np.ones((len(testArr), 1)))
+    print('测试集的错误率:%.3f%%' % float(errArr[predictions != np.mat(testLabelArr).T].sum() / len(testArr) * 100))
+    
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+if __name__ == '__main__':
+    dataArr, classLabels = loadDataSet('horseColicTraining2.txt')
+    testArr, testLabelArr = loadDataSet('horseColicTest2.txt')
+    bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth = 2), algorithm = "SAMME", n_estimators = 10)
+    bdt.fit(dataArr, classLabels)
+    predictions = bdt.predict(dataArr)
+    errArr = np.mat(np.ones((len(dataArr), 1)))
+    print('训练集的错误率:%.3f%%' % float(errArr[predictions != classLabels].sum() / len(dataArr) * 100))
+    predictions = bdt.predict(testArr)
+    errArr = np.mat(np.ones((len(testArr), 1)))
+    print('测试集的错误率:%.3f%%' % float(errArr[predictions != testLabelArr].sum() / len(testArr) * 100))
+    
+def plotROC(predStrengths, classLabels):
+    """
+    绘制ROC
+    Parameters:
+        predStrengths - 分类器的预测强度
+        classLabels - 类别
+    Returns:
+        无
+    """
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+    cur = (1.0, 1.0)                                                         #绘制光标的位置
+    ySum = 0.0                                                                 #用于计算AUC
+    numPosClas = np.sum(np.array(classLabels) == 1.0)                        #统计正类的数量
+    yStep = 1 / float(numPosClas)                                             #y轴步长   
+    xStep = 1 / float(len(classLabels) - numPosClas)                         #x轴步长
+
+    sortedIndicies = predStrengths.argsort()                                 #预测强度排序
+
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0; delY = yStep
+        else:
+            delX = xStep; delY = 0
+            ySum += cur[1]                                                     #高度累加
+        ax.plot([cur[0], cur[0] - delX], [cur[1], cur[1] - delY], c = 'b')     #绘制ROC
+        cur = (cur[0] - delX, cur[1] - delY)                                 #更新绘制光标的位置
+    ax.plot([0,1], [0,1], 'b--')
+    plt.title('AdaBoost马疝病检测系统的ROC曲线', FontProperties = font)
+    plt.xlabel('假阳率', FontProperties = font)
+    plt.ylabel('真阳率', FontProperties = font)
+    ax.axis([0, 1, 0, 1])
+    print('AUC面积为:', ySum * xStep)                                         #计算AUC
+    plt.show()
+    
+if __name__ == '__main__':
+    dataArr, LabelArr = loadDataSet('horseColicTraining2.txt')
+    weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, classLabels)
+    plotROC(aggClassEst.T, LabelArr)
